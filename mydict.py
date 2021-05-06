6ecd627c30fbbc646de7128d96a67eba93267184
ali-kaso-94 <72467561+ali-kaso-94@users.noreply.github.com>
2021-05-07 02:27:55 +0300

    commenting for better code comprehension
    basic code commenting

--- mydict.py
+++ mydict.py
@@ -6,20 +6,20 @@ utilizing python features and modules.
 """
 import json
 from difflib import get_close_matches as gcm
-# in case we needed the value of similarity between two words scale [0,1]
+# in case we needed the value of similarity between two words scale [0,1] (?future upgrade?)
 #from difflib import SequenceMatcher as SM
 
-data = json.load(open("data.json"))
+data = json.load(open("data.json"))                         #importing the json file as a python dictionary data structure
 
 def translate(word):
-        word = word.lower()
+        word = word.lower()                                 #lowering CAPITALIZATION if inputted by user
         if word in data:
             return data[word]
-        elif word.title() in data:
+        elif word.title() in data:                          #using title() method to search for words with the first letter capitalized like (Names,Countries ..etc)
             return data[word.title()]
-        elif word.upper() in data:
+        elif word.upper() in data:                          #acronym input handling (USA, FBI, NATO ...etc)
             return data[word.upper()]
-        elif len(gcm(word, data.keys(), cutoff=0.8)) > 0:
+        elif len(gcm(word, data.keys(), cutoff=0.8)) > 0:               #checking similarity in case user misspelled the word
             print("Did you mean %s instead?" %gcm(word, data.keys())[0])
             yn = input("press 'Y' or 'N': ")
             if yn == 'Y' or yn =='y':
@@ -38,9 +38,9 @@ def translate(word):
 word = input("Enter a Word: ")
 
 output= translate(word)
-if type(output) == list:
+if type(output) == list:                                        #for better display if the word has more than one meaning
     total_lines = len(output)
     for line, line_num in zip(output, range(total_lines)):
         print(f"{line_num +1}- {line}")
-else:
+else:                                                            #TODO: learning UI, GUI and more advanced python to develop this and other projects :)
     print(output)
