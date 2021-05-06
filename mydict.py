1aea5157be1772b428abb11d2aff3e133cb57fb0
ali-kaso-94 <72467561+ali-kaso-94@users.noreply.github.com>
2021-05-07 02:20:23 +0300

    Add files via upload

--- mydict.py
+++ mydict.py
@@ -6,37 +6,41 @@ utilizing python features and modules.
 """
 import json
 from difflib import get_close_matches as gcm
-from difflib import SequenceMatcher as SM
+# in case we needed the value of similarity between two words scale [0,1]
+#from difflib import SequenceMatcher as SM
 
 data = json.load(open("data.json"))
 
 def translate(word):
-    word = word.lower()
-
-    if word in data:
-        return data[word]
-
-    elif len(gcm(word, data.keys(), cutoff=0.8)) > 0:
-        print("Did you mean %s instead?" %gcm(word, data.keys())[0])
-        yn = input("press 'Y' or 'N': ")
-        if yn == 'Y' or yn =='y':
-            return data[gcm(word, data.keys())[0]]
-        elif yn =='n' or yn =='N':
-             word = input("Enter a new word: ")
-             return translate(word)
+        word = word.lower()
+        if word in data:
+            return data[word]
+        elif word.title() in data:
+            return data[word.title()]
+        elif word.upper() in data:
+            return data[word.upper()]
+        elif len(gcm(word, data.keys(), cutoff=0.8)) > 0:
+            print("Did you mean %s instead?" %gcm(word, data.keys())[0])
+            yn = input("press 'Y' or 'N': ")
+            if yn == 'Y' or yn =='y':
+                return data[gcm(word, data.keys())[0]]
+            elif yn =='n' or yn =='N':
+                 new = input("Enter a new word: ")
+                 return translate(new)
+            else:
+                return "The word doesn't exist, please double check it."
         else:
-            return "The word doesn't exist, please double check it."
-    else:
-        return "The word doesn't exist, please double check it."
+            print("The word doesn't exist, please double check it.")
+            new = input("Enter a new word: ")
+            return translate(new)
 
 
 word = input("Enter a Word: ")
 
 output= translate(word)
-
 if type(output) == list:
-    lines=len(output)
-    for i,item in zip(output,range(1,lines)):
-        print(f"{item}-{i}")
+    total_lines = len(output)
+    for line, line_num in zip(output, range(total_lines)):
+        print(f"{line_num +1}- {line}")
 else:
     print(output)
